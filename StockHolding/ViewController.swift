8ab1b3f2c840677f3573860354dcfca5427fee9a
Vinayv <vinayv@administrators-imac-4.local>
2016-10-26 15:07:18 +0530

    ccc

--- StockHolding/ViewController.swift
+++ StockHolding/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //changes
     }
 
     @IBAction func DashBoardViewAction(sender: AnyObject)
