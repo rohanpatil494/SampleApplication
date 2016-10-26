8c2e3789b854fa49b08fab8c34d0b70ee4a7ef8f
Rohan Patil <rohanp@hgs-isg-mac12.local>
2016-10-26 14:39:42 +0530

    new commit

---
+++ StockHolding/DashboardViewController.swift
@@ -0,0 +1,88 @@
+//
+//  DashboardViewController.swift
+//  StockHolding
+//
+//  Created by Rohan Patil on 26/10/16.
+//  Copyright © 2016 Rohan Patil. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+    
+    var titleArray = ["Demat","Mutual Funds","National Pension Scheme","IPO / Bonds / FD","Gold Rush","Services"]
+    //var imagesArray = ["a.png","b.png","c.png","d.png"]
+    var timer = NSTimer()
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        createNavigationBar()
+    }
+    
+    func createNavigationBar(){
+        
+        self.navigationItem.setHidesBackButton(true, animated: false)
+        self.title = "SHCIL Products"
+        
+        self.navigationController!.navigationBar.titleTextAttributes = [NSFontAttributeName: UIFont(name: "Helvetica", size: 22)! , NSForegroundColorAttributeName:UIColor.whiteColor()]
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 64/255.0, green: 111/255.0, blue: 214/255.0, alpha: 1.0)
+        
+        
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! CustomCollectionViewCell
+        
+        //cell.moduleIcon.image = UIImage(named: imagesArray[indexPath.row])
+        cell.moduleIcon.backgroundColor = UIColor.blueColor()
+        cell.nameLabel.text = titleArray[indexPath.row]
+        
+        
+        return cell
+    }
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath){
+        //14 mar
+        //let cell : CustomCollectionViewCell = collectionView.cellForItemAtIndexPath(indexPath) as! CustomCollectionViewCell
+        
+        
+        if indexPath.row == 0
+        {
+ //           let summaryController = self.storyboard?.instantiateViewControllerWithIdentifier("summary_view") as! SummaryViewController
+//            self.navigationController?.pushViewController(summaryController, animated: true)
+        }
+        else if indexPath.row == 1
+        {
+ //           let lastTransactionController = self.storyboard?.instantiateViewControllerWithIdentifier("last_trans_view") as! LastTransactionController
+ //           self.navigationController?.pushViewController(lastTransactionController, animated: true)
+        }
+        else if indexPath.row == 2
+        {
+//            self.indicator.startAnimating()
+            
+//            let wsm : WebserviceClass = WebserviceClass.sharedInstance
+//            wsm.delegates=self
+//            wsm.getReedemGift()
+            
+//            timer = NSTimer.scheduledTimerWithTimeInterval(9.0, target: self, selector: #selector(HomeViewController.someSelector), userInfo: nil, repeats: false)
+            //timer.fire()
+            
+        }
+        else
+        {
+//            let RedeemptionViewController = self.storyboard?.instantiateViewControllerWithIdentifier("redumption_view") as! RedumptionStatusController
+//            self.navigationController?.pushViewController(RedeemptionViewController, animated: true)
+        }
+    }
+    
+}
+
--- StockHolding/ViewController.swift
+++ StockHolding/ViewController.swift
@@ -10,17 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //changes
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    override func viewWillAppear(animated: Bool) {
+        self.navigationController?.navigationBarHidden = true
+    }
+    override func viewWillDisappear(animated: Bool) {
+        self.navigationController?.navigationBarHidden = false
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func DashBoardViewAction(sender: AnyObject)
+    {
+        let dashboardController = self.storyboard?.instantiateViewControllerWithIdentifier("Dashboard_View") as! DashboardViewController
+        self.navigationController?.pushViewController(dashboardController, animated: true)
+    }
 
 }
 
